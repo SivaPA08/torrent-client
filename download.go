c7e843adc08f41c5568ca284189783617eccf636
Siva <sivaprasada2004@gmail.com>
2026-07-11 20:47:10 +0530

    download

--- download.go
+++ download.go
@@ -1,29 +1,38 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"io"
 	"net/http"
 )
 
-func DownloadPiece(url string, start int64, end int64) ([]byte, error) {
-	client := &http.Client{}
+func DownloadPiece(url string, start, end int64) ([]byte, error) {
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		return []byte{}, errors.New("New error has came while downloading")
+		return nil, err
 	}
+
 	req.Header.Set(
 		"Range",
 		fmt.Sprintf("bytes=%d-%d", start, end),
 	)
-	resp, err := client.Do(req)
+
+	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return nil, err
 	}
 	defer resp.Body.Close()
-	if resp.StatusCode != http.StatusPartialContent && resp.StatusCode != http.StatusOK {
-		return nil, errors.New("IDK some error came in download")
+
+	if resp.StatusCode != http.StatusPartialContent &&
+		resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("unexpected status: %s", resp.Status)
 	}
-	return io.ReadAll(resp.Body)
+
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println("Received", len(data), "bytes")
+	return data, nil
 }
