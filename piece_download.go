3393c9636074fb0893cbb2ab1c8c97493ff1084d
Siva <sivaprasada2004@gmail.com>
2026-07-11 21:52:03 +0530

    concurrent download

--- piece_download.go
+++ piece_download.go
@@ -4,7 +4,22 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"sync"
 )
+
+func goRoutine(url string, start int64, end int64, file *os.File, wg *sync.WaitGroup) {
+	defer wg.Done()
+	data, err := DownloadPiece(url, start, end)
+	if err != nil {
+		fmt.Println("Download Failed")
+		return
+	}
+	_, err = file.WriteAt(data, 0)
+	if err != nil {
+		fmt.Println("Error while writting file")
+	}
+
+}
 
 func PieceDownload(torrent TorrentInfo) {
 	file, err := os.OpenFile(
@@ -17,22 +32,26 @@ func PieceDownload(torrent TorrentInfo) {
 	}
 	defer file.Close()
 	url := strings.TrimRight(torrent.WebSeedList[0], "/") + "/" + torrent.Name
-	start := int64(0)
-	end := torrent.PieceLength - 1
+	// start := int64(0)
+	// end := torrent.PieceLength - 1
 	//@dbg
-	fmt.Println("Piece Length:", torrent.PieceLength)
-	fmt.Println("start:", start)
-	fmt.Println("end:", end)
+	// fmt.Println("Piece Length:", torrent.PieceLength)
+	// fmt.Println("start:", start)
+	// fmt.Println("end:", end)
 	//@dbg
 
-	data, err := DownloadPiece(url, start, end)
-	if err != nil {
-		return
-	}
+	var wg sync.WaitGroup
+	no_of_pieces := int64(len(torrent.Pieces))
+	for i := int64(0); i < no_of_pieces; i++ {
+		wg.Add(1)
+		start := i * torrent.PieceLength
+		end := start + torrent.PieceLength - 1
+		if end >= torrent.TotalLength {
+			end = torrent.TotalLength - 1
+		}
 
-	_, err = file.WriteAt(data, 0)
-	if err != nil {
-		return
+		go goRoutine(url, start, end, file, &wg)
 	}
+	wg.Wait()
 
 }
